987ac348a1533e7ad101d60077beebfcd2b8d7a9
fioosa <firoosa123@gmail.com>
2025-12-18 09:37:07 +0530

    filtering, search, ordering

--- library/views.py
+++ library/views.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.shortcuts import render
-from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets, filters
 from rest_framework.decorators import api_view
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 from rest_framework.response import Response
@@ -35,6 +36,16 @@ def api_overview(request):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
     permission_classes = [IsStaffOrReadOnly]
 
 
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter
+    ]
+
+    filterset_fields = ['author', 'published_date']
+    search_fields = ['title', 'author']
+    ordering_fields = ['price', 'published_date']
