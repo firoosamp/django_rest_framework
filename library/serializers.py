f6492fb76d2e1cfb0958a04d665c4f50983c6a29
fioosa <firoosa123@gmail.com>
2025-12-18 12:03:58 +0530

    nested serializers

--- library/serializers.py
+++ library/serializers.py
@@ -2,14 +2,21 @@ from datetime import date
 
 from rest_framework import serializers
 
-from library.models import Book
+from library.models import Book, Author
+
+
+class AutherSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = ['id', 'name', 'email', 'birth_date']
 
 
 class BookSerializer(serializers.ModelSerializer):
+    author_details = AutherSerializer(source='author' , read_only= True)
     age = serializers.SerializerMethodField()
     class Meta:
         model = Book
-        fields = ['id', 'title', 'author', 'published_date', 'isbn', 'price' , 'age']
+        fields = ['id', 'title', 'author', 'published_date', 'isbn', 'price' , 'age' , 'author_details']
 
     def get_age(self, obj):
         return date.today().year - obj.published_date.year
@@ -20,3 +27,4 @@ class BookSerializer(serializers.ModelSerializer):
                 "ISBN must be exactly 10 or 13 characters"
             )
         return value
+
