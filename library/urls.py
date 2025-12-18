1c9fa0b22680636d3945bf77c74a70f3080e02b5
fioosa <firoosa123@gmail.com>
2025-12-18 05:57:08 +0530

    authentication and permissions

--- library/urls.py
+++ library/urls.py
@@ -1,15 +1,17 @@
 from django.db import router
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from library.views import api_overview, get_books, BookViewSet
+from library.views import api_overview,  BookViewSet
 
 router = DefaultRouter()
 router.register(r'books', BookViewSet, basename='BookViewSet')
 
 urlpatterns = [
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/',api_overview),
-    path('get_books', get_books),
 
     path("", include(router.urls))
 ]
--- library/views.py
+++ library/views.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.decorators import api_view
+from rest_framework.permissions import BasePermission, SAFE_METHODS
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
@@ -11,15 +12,22 @@ from library.serializers import BookSerializer
 
 # Create your views here.
 
+class IsStaffOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return True
+        return request.user and request.user.is_staff
+
+
 @api_view(['GET'])
 def api_overview(request):
     return Response({"massage": "Wellcome to Library API"})
 
-@api_view(['GET'])
-def get_books(request):
-    books = Book.objects.all()
-    serializer = BookSerializer(books, many=True)
-    return Response(serializer.data)
+# @api_view(['GET'])
+# def get_books(request):
+#     books = Book.objects.all()
+#     serializer = BookSerializer(books, many=True)
+#     return Response(serializer.data)
 
 
 # ...................ViewSet..........................
@@ -27,4 +35,6 @@ def get_books(request):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsStaffOrReadOnly]
+
 
