114cde132eba65b5932e4f70c3c60841bd1b4633
fioosa <firoosa123@gmail.com>
2025-12-18 12:27:36 +0530

    file uploades

--- library/models.py
+++ library/models.py
@@ -17,6 +17,7 @@ class Book(models.Model):
     published_date = models.DateField()
     isbn =models.CharField(max_length=13, unique=True)
     price = models.DecimalField(max_digits=6,decimal_places=2)
+    cover_image = models.ImageField(upload_to='book_covers/', null=True, blank=True)
     def __str__(self):
         return self.title
 
--- library/urls.py
+++ library/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.db import router
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
@@ -15,3 +17,6 @@ urlpatterns = [
 
     path("", include(router.urls))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
